50851a475e8c7476f26cded4fb18db2c9d5b8243
aayush <sharma31aayush@gmail.com>
2022-12-14 23:13:06 +0530

    added saveFavourites and removefromFavourites functions

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
     const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=5891a377`;
     const response = await fetch(url);
     const responseJson = await response.json();
-    // console.log(responseJson);
 
     //responseJson.Search as Search on json contains the movie info(posters)
     if (responseJson.Search) {
@@ -26,6 +27,17 @@ function App() {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  const addFavouritesmovie = (movie) => {
+    const newFavouritesList = [...favourites, movie];
+    setFavourites(newFavouritesList);
+  };
+
+  const removeFavouritesMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+    setFavourites(newFavouriteList);
+  };
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -33,7 +45,21 @@ function App() {
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
-        <MovieList movies={movies} favouriteComponent={AddFavourites} />
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouritesmovie}
+          favouriteComponent={AddFavourites}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouritesMovie}
+          favouriteComponent={RemoveFavourites} //shows remove from fav in overlay div the text.
+        />
       </div>
     </div>
   );

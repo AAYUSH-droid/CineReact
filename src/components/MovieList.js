41231d1d9916c6d7f9189b432b1553c3ddbc61fa
aayush <sharma31aayush@gmail.com>
2022-12-14 18:07:42 +0530

    added add to favourites functionality

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const MovieList = (props) => {
+  const FavouriteComponent = props.favouriteComponent;
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div>
+        <div className="image-cointainer d-flex justify-content-start m-3">
           <img src={movie.Poster} alt="movie"></img>
+          <div className="overlay d-flex align-items-center justify-content-center">
+            <FavouriteComponent />
+          </div>
         </div>
       ))}
     </>
